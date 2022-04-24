e189701616c68a7bcbad2af19e5d5621d4f53110
U-C-S <ucs3721@outlook.com>
2022-04-24 22:43:52 +0530

    add more properties to Browser model

--- src/browser.rs
+++ src/browser.rs
@@ -3,16 +3,42 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 pub struct Browser {
     pub full_name: String,
-    pub short_name: String,
+    pub short_name: Option<String>,
+
+    /// The path to the browser's executable.
     pub path: String,
+
+    /// Type of Browser
+    pub btype: BrowserType,
+
+    /// Icons from https://github.com/alrra/browser-logos/tree/main/src
+    ///
+    /// We just use the name of the foider
+    pub icon: String,
 }
 
 impl Browser {
     pub fn new(full_name: String, short_name: String, path: String) -> Self {
         Self {
             full_name,
-            short_name,
             path,
+            btype: BrowserType::Unknown,
+            short_name: Some(short_name),
+            icon: "".to_string(),
         }
     }
 }
+
+#[derive(Deserialize, Debug)]
+pub enum BrowserType {
+    Chromium,
+    Firefox,
+    Unknown,
+}
+
+// Google Chrome
+// Mozilla Firefox
+// Microsoft Edge
+// Opera
+// Vivaldi
+// Brave
